940f7e1f9e23fe3ddc0971ed10a11dd778cbe5cd
Михаил Мясников <gutufik@mail.ru>
2022-01-24 12:34:51 +0300

    updated pages

---
+++ MobileApp/MobileApp/ProjectsPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProjectsPage : ContentPage
+    {
+        public List<string> Projects { get; set; }
+        public ProjectsPage()
+        {
+            InitializeComponent();
+            Projects = new List<string>();
+            FillList();
+            this.BindingContext = this;
+        }
+
+        public void FillList()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Projects.Add($"Проект {i + 1}");
+            }
+        }
+
+        private async void lwProjectsItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            await Navigation.PushAsync(new SelectedProjectPage(lwProjects.SelectedItem.ToString()));
+        }
+    }
+}
--- MobileApp/MobileApp/RegisterPage.xaml.cs
+++ MobileApp/MobileApp/RegisterPage.xaml.cs
@@ -1,28 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace MobileApp
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
     {
-        public MainPage()
+        public RegisterPage()
         {
             InitializeComponent();
-
         }
         private async void btnSignUpClicked(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new RegisterPage());
-        }
-
-        private async void btnLoginClicked(object sender, EventArgs e)
-        {
-            await Navigation.PushAsync(new ProjectsPage());
+            await Navigation.PopAsync();
         }
     }
 }
